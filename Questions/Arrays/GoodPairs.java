c5a1890ed439fda182307965fa5c618d01522637
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-09-09 12:34:53 +0530

    Solved 2 another problems

---
+++ Questions/Arrays/GoodPairs.java
@@ -0,0 +1,22 @@
+package Questions.Arrays;
+
+// https://leetcode.com/problems/number-of-good-pairs/
+public class GoodPairs {
+ public int numIdenticalPairs(int[] nums) {
+  int count = 0;
+
+  for (int i = 0; i < nums.length; i++) {
+
+   for (int j = i + 1; j < nums.length; j++) {
+
+    if (nums[i] == nums[j]) {
+     count++;
+    }
+
+   }
+
+  }
+
+  return count;
+ }
+}
