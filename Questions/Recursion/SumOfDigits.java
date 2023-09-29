7e21e660c13e884069fb57b1848fdc182c7a61e5
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-09-29 13:50:48 +0530

    solved some recursion questions

---
+++ Questions/Recursion/SumOfDigits.java
@@ -0,0 +1,15 @@
+package Questions.Recursion;
+
+public class SumOfDigits {
+
+ public static int sumOfDigits(int n) {
+  if (n == 0)
+   return 0;
+
+  return n % 10 + sumOfDigits(n / 10);
+ }
+
+ public static void main(String[] args) {
+  System.out.println(sumOfDigits(458));
+ }
+}
