3fb8c5e463018713f87c335b1d08a8cdb319e130
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-10-06 19:41:21 +0530

    Solved Magic Number

---
+++ Questions/BitManipulation/MagicNumber.java
@@ -0,0 +1,18 @@
+package Questions.BitManipulation;
+
+public class MagicNumber {
+ public static void main(String[] args) {
+  int n = 6;
+
+  int ans = 0;
+  int base = 5;
+  while (n > 0) {
+   int lastBit = n & 1;
+   n = n >> 1;
+   ans += lastBit * base;
+   base *= 5;
+  }
+
+  System.out.println(ans);
+ }
+}
