66494b0613cc690484d640d9d1a4f31f6303c110
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-10-06 22:50:54 +0530

    printed more triangles from recursion

--- Questions/Recursion/Patterns/Traingle.java
+++ Questions/Recursion/Patterns/Traingle.java
@@ -11,7 +11,9 @@ public class Traingle {
  public static void main(String[] args) {
   printTriangle(5, 0);
   System.out.println();
-  printTriangle(5, 0);
+  printTriangle(5);
+  System.out.println();
+  printTriangle2(5, 0);
  }
 
  public static void printTriangle(int row, int column) {
@@ -24,6 +26,30 @@ public class Traingle {
   } else {
    System.out.println();
    printTriangle(row - 1, 0);
+  }
+ }
+
+ public static void printTriangle(int row) {
+  if (row == 0) {
+   return;
+  }
+  for (int i = 0; i < row; i++) {
+   System.out.print("*");
+  }
+  System.out.println();
+  printTriangle(row - 1);
+ }
+
+ public static void printTriangle2(int row, int column) {
+  if (row == 0) {
+   return;
+  }
+  if (column < row) {
+   printTriangle2(row, column + 1);
+   System.out.print("*");
+  } else {
+   printTriangle2(row - 1, 0);
+   System.out.println();
   }
  }
 
