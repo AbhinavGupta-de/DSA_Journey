eb828aea451009362bbf420fb54c80051ed0f077
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-10-06 20:17:02 +0530

    solved two power related questions

---
+++ Questions/BitManipulation/PowerOfTwo.java
@@ -0,0 +1,12 @@
+package Questions.BitManipulation;
+
+public class PowerOfTwo {
+ public static void main(String[] args) {
+  int n = 16;
+  System.out.println("Is " + n + " power of two? " + isPowerOfTwo(n));
+ }
+
+ public static boolean isPowerOfTwo(int n) {
+  return (n & (n - 1)) == 0;
+ }
+}
