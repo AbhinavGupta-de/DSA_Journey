aaf0464134767358b53fd9810ce3402dd435c5ba
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-10-09 22:29:15 +0530

    A small warming solved

--- Questions/Arrays/HighestAltitude.java
+++ Questions/Arrays/HighestAltitude.java
@@ -6,7 +6,6 @@ package Questions.Arrays;
 public class HighestAltitude {
  public int largestAltitude(int[] gain) {
   int max = 0;
-  int sum = 0;
   // for (int i = 0; i < gain.length; i++) {
   // sum += gain[i];
   // if (sum > max) {
