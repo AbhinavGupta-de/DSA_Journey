4710dc4c227f8d5cedf4d988d7f291e9d9120965
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-10-24 16:31:44 +0530

    solved lucky number

--- Questions/Arrays/LuckyNumberInMatrix.java
+++ Questions/Arrays/LuckyNumberInMatrix.java
@@ -6,13 +6,26 @@ public class LuckyNumberInMatrix {
  public static List<Integer> luckyNumbers(int[][] matrix) {
   List<Integer> result = new ArrayList<>();
 
-  List<Integer> minRow = new ArrayList<>();
-  // Brute Force - O(n^2)
   for (int i = 0; i < matrix.length; i++) {
-   for (int j = 0; j < matrix[i].length; j++) {
-    if (matrix[i][j] < matrix[i][j + 1]) {
-     minRow.add(matrix[i][j]);
+   int minRow = matrix[i][0];
+   int colIndex = 0;
+   for (int j = 1; j < matrix[i].length; j++) {
+    if (matrix[i][j] < minRow) {
+     minRow = matrix[i][j];
+     colIndex = j;
     }
+   }
+
+   boolean isMaxCol = true;
+   for (int k = 0; k < matrix.length; k++) {
+    if (matrix[k][colIndex] > minRow) {
+     isMaxCol = false;
+     break;
+    }
+   }
+
+   if (isMaxCol) {
+    result.add(minRow);
    }
   }
 
