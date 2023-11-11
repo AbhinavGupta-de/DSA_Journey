65e7cd3d34af76e7db89f9499088bcd4f1eb3adf
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-11-11 14:05:14 +0530

    solved nth magical number

--- Questions/Searching/HardQuestions/NthMagicalNumber.java
+++ Questions/Searching/HardQuestions/NthMagicalNumber.java
@@ -1,7 +1,34 @@
 package Questions.Searching.HardQuestions;
 
 public class NthMagicalNumber {
- public int nthMagicalNum(int n, int a, int b) {
-  return -1;
+ public long nthMagicalNum(int n, int a, int b) {
+  long min = Math.min(a, b);
+
+  long start = min;
+  long end = n * min;
+
+  while (start < end) {
+   long mid = start + (end - start) / 2;
+   if (count(mid, a, b) < n)
+    start = mid + 1;
+   else
+    end = mid;
+  }
+
+  return start % 1000000007;
+ }
+
+ public long count(long num, long a, long b) {
+  return (num / a) + (num / b) - (num / lcm(a, b));
+ }
+
+ public long lcm(long a, long b) {
+  return (a * b) / gcd(a, b);
+ }
+
+ public long gcd(long a, long b) {
+  if (b == 0)
+   return a;
+  return gcd(b, a % b);
  }
 }
