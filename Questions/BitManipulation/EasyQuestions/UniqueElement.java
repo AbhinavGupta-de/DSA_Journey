b5415f8de7e6388d2a91878839406b6e5791a93c
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-11-22 21:26:09 +0530

    solved more uniquelements problems

--- Questions/BitManipulation/EasyQuestions/UniqueElement.java
+++ Questions/BitManipulation/EasyQuestions/UniqueElement.java
@@ -4,10 +4,11 @@ package Questions.BitManipulation.EasyQuestions;
 
 public class UniqueElement {
  public static void main(String[] args) {
-  int[] arr = { 1, 2, 3, 4, 1, 2, 3 };
-  System.out.println(uniqueElement(arr));
+  int[] arr = { 1, 1, 1, 2, 2, 3, 4, 4, 4, 5 };
+  uniqueElement3(arr);
  }
 
+ // If there is only one unique element in the array
  public static int uniqueElement(int[] arr) {
   // using Bit Manipulation
   int unique = 0;
@@ -15,22 +16,46 @@ public class UniqueElement {
    unique ^= arr[i];
   }
   return unique;
+ }
 
-  // Using hashing
+ // If there are multiple unique elements in the array
+ public static void uniqueElement2(int[] arr) {
+  // using Bit Manipulation
+  int xor = 0;
+  for (int i = 0; i < arr.length; i++) {
+   xor ^= arr[i];
+  }
+  int mask = xor & -xor;
+  int x = 0;
+  int y = 0;
 
-  // HashSet<Integer> set = new HashSet<>();
-  // HashSet<Integer> unique = new HashSet<>();
+  for (int i = 0; i < arr.length; i++) {
+   if ((arr[i] & mask) == mask) {
+    x ^= arr[i];
+   } else {
+    y ^= arr[i];
+   }
+  }
+  System.out.println(x + " " + y);
+ }
 
-  // for (int i = 0; i < arr.length; i++) {
-  // if (set.contains(arr[i])) {
-  // unique.remove(arr[i]);
-  // } else {
-  // set.add(arr[i]);
-  // unique.add(arr[i]);
-  // }
-  // }
+ // If all the elements appea thrice except one element
+ public static void uniqueElement3(int[] arr) {
+  // using Bit Manipulation
+  int ans = 0;
+  for (int i = 0; i < 32; i++) {
+   int count = 0;
+   int mask = (1 << i);
+   for (int j = 0; j < arr.length; j++) {
+    if ((arr[j] & mask) != 0) {
+     count++;
+    }
+   }
+   if (count % 3 != 0) {
+    ans |= mask;
+   }
+  }
 
-  // return unique.iterator().next();
-
+  System.out.println(ans);
  }
 }
