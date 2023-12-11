c7b3bceedb02f793794680dfb67672c686f7eee0
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-12-11 17:29:26 +0530

    made a few changes to trapping watter problem

--- Questions/Arrays/HardQuestions/TrappingRainWater.java
+++ Questions/Arrays/HardQuestions/TrappingRainWater.java
@@ -2,25 +2,37 @@ package Questions.Arrays.HardQuestions;
 
 public class TrappingRainWater {
  public int trap(int[] nums) {
+
   int n = nums.length;
   int water = 0;
   int leftMax = 0;
   int leftMaxIndex = 0;
   int rightMax = 0;
+
   for (int i = 0; i < n; i++) {
+
    if (nums[i] > leftMax) {
+
     leftMax = nums[i];
     leftMaxIndex = i;
+
    } else {
+
     water += leftMax - nums[i];
+
    }
   }
 
   for (int i = n - 1; i > leftMaxIndex; i--) {
+
    if (nums[i] > rightMax) {
+
     rightMax = nums[i];
+
    } else {
+
     water += rightMax - nums[i];
+
    }
   }
 
