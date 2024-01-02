99054283c60a0620a922fd2f4c5d6387110bb9cb
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-02 09:20:53 +0530

    remove Element

---
+++ Questions/Arrays/EasyQuestions/RemoveElement.java
@@ -0,0 +1,22 @@
+package questions.arrays.easyquestions;
+
+import java.util.Arrays;
+
+/**
+ * RemoveElement
+ */
+public class RemoveElement {
+
+ public int removeElement(int[] nums, int val) {
+  int count = 0;
+  int length = nums.length;
+  for (int i = 0; i < length; i++) {
+   if (nums[i] == val) {
+    count++;
+    nums[i] = Integer.MAX_VALUE;
+   }
+  }
+  Arrays.sort(nums);
+  return length - count;
+ }
+}
