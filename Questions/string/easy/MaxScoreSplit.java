1c34b29951f49712f3e86f364378ee55de527001
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-02 09:50:34 +0530

    optimized the solution

--- Questions/string/easy/MaxScoreSplit.java
+++ Questions/string/easy/MaxScoreSplit.java
@@ -1,41 +1,32 @@
 package questions.string.easy;
 
-import java.util.Arrays;
-
 public class MaxScoreSplit {
+
  public static int maxScore(String s) {
-  int[] preffixSum = new int[s.length()];
+  int[] prefixSum = new int[s.length()];
   int[] suffixSum = new int[s.length()];
 
-  if (Integer.parseInt(s.charAt(0) + "") == 1) {
-   preffixSum[0] = 0;
-  } else {
-   preffixSum[0] = 1;
+  // Initialize prefix sum
+  prefixSum[0] = s.charAt(0) == '0' ? 1 : 0;
+
+  // Calculate prefix sum
+  for (int i = 1; i < s.length(); i++) {
+   prefixSum[i] = prefixSum[i - 1] + (s.charAt(i) == '0' ? 1 : 0);
   }
 
+  // Initialize suffix sum
   suffixSum[s.length() - 1] = 0;
 
-  for (int i = 1; i < s.length(); i++) {
-   preffixSum[i] = preffixSum[i - 1];
-   int digit = Integer.parseInt(s.charAt(i) + "");
-   if (digit == 0) {
-    preffixSum[i] += digit + 1;
-   }
-  }
-
+  // Calculate suffix sum
   for (int i = s.length() - 2; i >= 0; i--) {
-   suffixSum[i] = suffixSum[i + 1] + Integer.parseInt(s.charAt(i + 1) + "");
+   suffixSum[i] = suffixSum[i + 1] + Character.getNumericValue(s.charAt(i + 1));
   }
-
-  System.out.println(Arrays.toString(preffixSum));
-  System.out.println(Arrays.toString(suffixSum));
 
   int maxScore = 0;
 
   for (int i = 0; i < s.length() - 1; i++) {
-   int score = preffixSum[i] + suffixSum[i];
-   if (score > maxScore)
-    maxScore = score;
+   int score = prefixSum[i] + suffixSum[i];
+   maxScore = Math.max(maxScore, score);
   }
 
   return maxScore;
