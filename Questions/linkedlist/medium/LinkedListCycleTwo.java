8db81218fe47f96e186606bd37270cce7707ed1d
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-04 19:19:32 +0530

    solved to detect where the cycle begins in linked list

--- Questions/linkedlist/medium/LinkedListCycleTwo.java
+++ Questions/linkedlist/medium/LinkedListCycleTwo.java
@@ -5,5 +5,29 @@ import questions.linkedlist.ListNode;
 public class LinkedListCycleTwo {
  public ListNode detectCycle(ListNode head) {
 
+  ListNode slow = head;
+  ListNode fast = head;
+  boolean cycle = false;
+
+  while (fast != null && fast.next != null) {
+
+   slow = slow.next;
+   fast = fast.next.next;
+
+   if (slow == fast) {
+    cycle = true;
+    break;
+   }
+  }
+
+  slow = head;
+  while (cycle) {
+   if (slow == fast)
+    return slow;
+
+   slow = slow.next;
+   fast = fast.next;
+  }
+  return null;
  }
 }
