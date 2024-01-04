5bc1d0f5b8c11f2c11a40306dc0b2205c53f1181
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-04 20:38:54 +0530

    solved simplify path

---
+++ stacks/medium/SimplifyPath.java
@@ -0,0 +1,28 @@
+package stacks.medium;
+
+public class SimplifyPath {
+ public String simplifyPath(String path) {
+
+  String[] paths = path.split("/");
+  StringBuilder stb = new StringBuilder();
+
+  for (String str : paths) {
+   if (str.isEmpty() || str.equals("."))
+    continue;
+
+   else if (str.equals("..")) {
+    if (stb.length() > 0) {
+     int lastIndex = stb.lastIndexOf("/");
+     stb.delete(lastIndex, stb.length());
+    }
+   } else {
+    stb.append("/").append(str);
+   }
+  }
+
+  if (stb.length() == 0)
+   stb.append("/");
+
+  return stb.toString();
+ }
+}
