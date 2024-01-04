416648011f6849907f1da42b49cbbdb9608b9e46
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-04 15:34:06 +0530

    Solved swap nodes in pairs

--- Questions/linkedlist/medium/SwapNodesInPairs.java
+++ Questions/linkedlist/medium/SwapNodesInPairs.java
@@ -4,8 +4,16 @@ import questions.linkedlist.ListNode;
 
 public class SwapNodesInPairs {
 
- public ListNode swapPairs() {
+ public ListNode swapPairs(ListNode head) {
+  if (head == null || head.next == null)
+   return head;
 
+  ListNode newNode = swapPairs(head.next.next);
+  ListNode modifiedHead = head.next;
+  head.next.next = head;
+  head.next = newNode;
+
+  return modifiedHead;
  }
 
 }
