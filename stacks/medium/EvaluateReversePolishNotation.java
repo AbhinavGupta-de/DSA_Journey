0f4fbef9e650aa1075aacfa0f317fe2f5f071517
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-04 20:57:18 +0530

    solved to evaluate reverse polish notation

---
+++ stacks/medium/EvaluateReversePolishNotation.java
@@ -0,0 +1,37 @@
+package stacks.medium;
+
+import java.util.*;
+
+public class EvaluateReversePolishNotation {
+ public int evalRPN(String[] A) {
+  Stack<Integer> stack = new Stack<>();
+
+  for (String i : A) {
+   if (i.equals("+")) {
+    int num1 = stack.pop();
+    int num2 = stack.pop();
+    stack.push(num1 + num2);
+   }
+
+   else if (i.equals("-")) {
+    int num2 = stack.pop();
+    int num1 = stack.pop();
+    stack.push(num1 - num2);
+   }
+
+   else if (i.equals("*")) {
+    int num1 = stack.pop();
+    int num2 = stack.pop();
+    stack.push(num1 * num2);
+   } else if (i.equals("/")) {
+    int num2 = stack.pop();
+    int num1 = stack.pop();
+    stack.push(num1 / num2);
+   } else {
+    stack.push(Integer.parseInt(i));
+   }
+  }
+
+  return stack.pop();
+ }
+}
