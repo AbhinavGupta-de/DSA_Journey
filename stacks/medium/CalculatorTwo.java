5958b369198f756cc5b6f1e53d35cb86e3b5fd46
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-15 16:50:03 +0530

    created infix to postfix converter

--- stacks/medium/CalculatorTwo.java
+++ stacks/medium/CalculatorTwo.java
@@ -1,7 +1,55 @@
 package stacks.medium;
 
+import java.util.Stack;
+
 public class CalculatorTwo {
+
+ public int precedence(char c) {
+  if (c == '^')
+   return 3;
+  else if (c == '*' || c == '/')
+   return 2;
+  else if (c == '+' || c == '-')
+   return 1;
+
+  return 0;
+ }
+
+ public String infixPostFix(String input) {
+  StringBuilder stb = new StringBuilder();
+  Stack<Character> stack = new Stack<>();
+
+  for (char c : input.toCharArray()) {
+
+   if (c >= 'a' && c <= 'z') {
+    stb.append(c);
+   } else if (c == '(')
+    stack.push(c);
+   else if (c == ')') {
+    while (!stack.isEmpty() && stack.peek() != '(') {
+     stb.append(c);
+     stack.pop();
+    }
+
+    if (!stack.isEmpty())
+     stack.pop();
+   } else {
+    while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+     stb.append(stack.pop());
+    }
+    stack.push(c);
+   }
+  }
+
+  while (!stack.isEmpty()) {
+   stb.append(stack.pop());
+  }
+
+  return stb.toString();
+ }
+
  public int calculate(String s) {
 
+  return 0;
  }
 }
