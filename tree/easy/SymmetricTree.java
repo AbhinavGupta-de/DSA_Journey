100f71b2e86333466173efdfd8a5e502d7af29d2
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-17 20:55:01 +0530

    solved symmetric trees

---
+++ tree/easy/SymmetricTree.java
@@ -0,0 +1,38 @@
+package tree.easy;
+
+import tree.TreeNode;
+
+public class SymmetricTree {
+
+ public boolean isSymmetric(TreeNode root) {
+
+  if (root == null)
+   return false;
+
+  return checker(root.left, root.right);
+
+ }
+
+ public boolean checker(TreeNode root1, TreeNode root2) {
+
+  if (root1 == null || root2 == null) {
+   if (root1 == null && root2 == null)
+    return true;
+
+   return false;
+  }
+
+  if (root1.val != root2.val)
+   return false;
+
+  if (!checker(root1.left, root2.right))
+   return false;
+
+  if (!checker(root1.right, root2.left))
+   return false;
+
+  return true;
+
+ }
+
+}
