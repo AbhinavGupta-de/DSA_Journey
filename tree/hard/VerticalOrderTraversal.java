0d8bac9e3757dbe95034969f68f987d99eb26e4d
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-23 21:50:42 +0530

    solved with hashmap now

--- tree/hard/VerticalOrderTraversal.java
+++ tree/hard/VerticalOrderTraversal.java
@@ -2,64 +2,70 @@ package tree.hard;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import tree.TreeNode;
 
 public class VerticalOrderTraversal {
 
- public List<List<Integer>> verticalTraversal(TreeNode root) {
-  TreeMap<Integer, TreeMap<Integer, List<Integer>>> map = new TreeMap<>();
-  verticalTraversalHelper(root, 0, 0, map);
+  int minDis = 0;
+  int maxDis = 0;
 
-  List<List<Integer>> result = new ArrayList<>();
+  public List<List<Integer>> verticalTraversal(TreeNode root) {
+    HashMap<Integer, HashMap<Integer, List<Integer>>> map = new HashMap<>();
+    verticalTraversalHelper(root, 0, 0, map);
 
-  for (Map.Entry<Integer, TreeMap<Integer, List<Integer>>> entry : map.entrySet()) {
-   List<Integer> vertLevel = new ArrayList<>();
-   TreeMap<Integer, List<Integer>> getMap = entry.getValue();
+    List<List<Integer>> result = new ArrayList<>();
 
-   for (List<Integer> list : getMap.values()) {
-    vertLevel.addAll(list);
-   }
+    for (int i = minDis; i <= maxDis; i++) {
+      HashMap<Integer, List<Integer>> getMap = map.get(i);
+      List<Integer> levelList = new ArrayList<>();
+      for (Integer key : getMap.keySet()) {
+        if (getMap.containsKey(key)) {
+          List<Integer> list = getMap.get(key);
+          levelList.addAll(list);
+        }
+      }
+      result.add(levelList);
+    }
 
-   result.add(vertLevel);
+    return result;
   }
 
-  return result;
- }
+  private void verticalTraversalHelper(TreeNode root, int level, int distance,
+      HashMap<Integer, HashMap<Integer, List<Integer>>> map) {
 
- private void verticalTraversalHelper(TreeNode root, int level, int distance,
-   TreeMap<Integer, TreeMap<Integer, List<Integer>>> map) {
+    if (root == null)
+      return;
 
-  if (root == null)
-   return;
+    minDis = Math.min(minDis, distance);
+    maxDis = Math.max(maxDis, distance);
 
-  if (map.containsKey(distance)) {
-   TreeMap<Integer, List<Integer>> getMap = map.get(distance);
+    if (map.containsKey(distance)) {
+      HashMap<Integer, List<Integer>> getMap = map.get(distance);
 
-   if (getMap.containsKey(level)) {
-    List<Integer> list = getMap.get(level);
-    list.add((Integer) root.val);
-    Collections.sort(list);
-    getMap.put(level, list);
-   } else {
-    List<Integer> list = new ArrayList<>();
-    list.add((Integer) root.val);
-    getMap.put(level, list);
-   }
-  } else {
-   List<Integer> list = new ArrayList<>();
-   list.add((Integer) root.val);
+      if (getMap.containsKey(level)) {
+        List<Integer> list = getMap.get(level);
+        list.add((Integer) root.val);
+        Collections.sort(list);
+        getMap.put(level, list);
+      } else {
+        List<Integer> list = new ArrayList<>();
+        list.add((Integer) root.val);
+        getMap.put(level, list);
+      }
+    } else {
+      List<Integer> list = new ArrayList<>();
+      list.add((Integer) root.val);
 
-   TreeMap<Integer, List<Integer>> getMap = new TreeMap<>();
-   getMap.put(level, list);
+      HashMap<Integer, List<Integer>> getMap = new HashMap<>();
+      getMap.put(level, list);
 
-   map.put(distance, getMap);
+      map.put(distance, getMap);
+    }
+
+    verticalTraversalHelper(root.left, level + 1, distance - 1, map);
+    verticalTraversalHelper(root.right, level + 1, distance + 1, map);
   }
-
-  verticalTraversalHelper(root.left, level + 1, distance - 1, map);
-  verticalTraversalHelper(root.right, level + 1, distance + 1, map);
- }
 }
