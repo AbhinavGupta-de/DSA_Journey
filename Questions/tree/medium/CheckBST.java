b782c0c07ec46bd732383461436d5a72c65fafb7
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-24 20:14:53 +0530

    tried solving the question

--- Questions/tree/medium/CheckBST.java
+++ Questions/tree/medium/CheckBST.java
@@ -19,4 +19,35 @@ public class CheckBST {
 
  }
 
+ private class Data {
+
+  long min;
+  long max;
+  TreeNode node;
+  boolean isValid;
+
+  Data(TreeNode node, long min, long max, boolean isValid) {
+   this.node = node;
+   this.min = min;
+   this.max = max;
+   this.isValid = isValid;
+  }
+
+ }
+
+ public Data isValid(TreeNode root) {
+  if (root == null)
+   return new Data(null, Long.MIN_VALUE, Long.MIN_VALUE, false);
+
+  Data l = isValid(root.left);
+  Data r = isValid(root.right);
+
+  if ((l.isValid == false || r.isValid == false) || ((Long) root.val >= r.min && (Long) root.val <= l.max)) {
+   return new Data(root, -1, -1, false);
+  }
+
+  return new Data(root, l.max, l.min, true);
+
+ }
+
 }
