247604aa3af15acf4a1d77297d6cec0005b66672
Abhinav Gupta <abhinav.gupta@gmail.com>
2024-01-31 11:33:36 +0530

    solved leetcode 2487

---
+++ questions/linkedlist/medium/RemoveNodesFromLinkedList.java
@@ -0,0 +1,25 @@
+package questions.linkedlist.medium;
+
+import questions.linkedlist.ListNode;
+
+import java.util.*;
+
+public class RemoveNodesFromLinkedList {
+
+ // 5 2 13 3 8
+ // 5 1 2 13 8 7 6 9
+
+ public ListNode removeNodes(ListNode head) {
+  if (head == null)
+   return head;
+
+  ListNode next = removeNodes(head.next);
+  if (next == null || next.val < head.val) {
+   head.next = next;
+   return head;
+  }
+
+  return next;
+ }
+
+}
