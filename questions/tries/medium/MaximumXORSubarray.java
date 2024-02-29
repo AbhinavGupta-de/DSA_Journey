5eefc555eef7933f191a32184fed0382b7c44c61
Abhinav Gupta <abhinav.gupta@gmail.com>
2024-02-29 09:53:01 +0530

    solved k smallest elements

--- questions/tries/medium/MaximumXORSubarray.java
+++ questions/tries/medium/MaximumXORSubarray.java
@@ -1,55 +1,50 @@
-package questions.tries.medium;
+// package questions.tries.medium;
 
-import java.util.HashMap;
+// import java.util.HashMap;
 
-public class MaximumXORSubarray {
+// public class MaximumXORSubarray {
 
- private class Trie {
+// private class Trie {
 
-  HashMap<Integer, Trie> map;
-  int index;
+// HashMap<Integer, Trie> map;
+// int index;
 
-  public Trie() {
-   map = new HashMap<>();
-   index = -1;
-  }
+// public Trie() {
+// map = new HashMap<>();
+// index = -1;
+// }
 
-  public void insert(int num, int index) {
-   Trie curr = this;
-   for (int i = 31; i >= 0; i--) {
-    int bit = ((num >> i) & 1);
-    if (!curr.map.containsKey(bit)) {
-     curr.map.put(bit, new Trie());
-    }
+// public void insert(int num, int index) {
+// Trie curr = this;
+// for (int i = 31; i >= 0; i--) {
+// int bit = ((num >> i) & 1);
+// if (!curr.map.containsKey(bit)) {
+// curr.map.put(bit, new Trie());
+// }
 
-    curr = curr.map.get(bit);
-   }
-   curr.index = index;
-  }
+// curr = curr.map.get(bit);
+// }
+// curr.index = index;
+// }
 
-  public int getMaxXOR(int num) {
+// public void getMaxXOR(int num) {
 
-   Trie curr = this;
+// Trie curr = this;
 
-   for(int i = 31; i >= 0; i-- {
-    
-   }
+// }
+// }
 
+// public int[] solve(int[] A) {
 
-  }
- }
+// // get the preffix XOR
+// private int[] preffixXOR = preffixXOR(A);
 
- public int[] solve(int[] A) {
+// private int max = Integer.MIN_VALUE;
 
-  // get the preffix XOR
-  private int[] preffixXOR = preffixXOR(A);
+// for (int i = 0; i < preffixXOR.length; i++) {
+// int max_xor = getMaxXOR(preffixXOR[i], root);
+// }
 
-  private int max = Integer.MIN_VALUE;
+// }
 
-  for (int i = 0; i < preffixXOR.length; i++) {
-   int max_xor = getMaxXOR(preffixXOR[i], root);
-  }
-
- }
-
-}
+// }
