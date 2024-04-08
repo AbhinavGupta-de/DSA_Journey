807b4c62c850874a87a1fb73f53f5a83a5a5c2f2
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-04-08 11:10:32 +0530

    solved unique path

---
+++ questions/dp/medium/UniquePath.java
@@ -0,0 +1,30 @@
+package dp.medium;
+
+import java.util.*;
+
+public class UniquePath {
+ public int uniquePaths(int m, int n) {
+  int[][] nums = new int[m][n];
+  for (int[] array : nums) {
+   Arrays.fill(array, -1);
+  }
+  return helper(m, n, 0, 0, nums);
+ }
+
+ private int helper(int m, int n, int i, int j, int[][] nums) {
+  if (i == m || j == n)
+   return 0;
+  if (i == m - 1 || j == n - 1)
+   return 1;
+
+  if (nums[i][j] != -1)
+   return nums[i][j];
+
+  int right = helper(m, n, i, j + 1, nums);
+  int down = helper(m, n, i + 1, j, nums);
+
+  nums[i][j] = right + down;
+  return right + down;
+
+ }
+}
