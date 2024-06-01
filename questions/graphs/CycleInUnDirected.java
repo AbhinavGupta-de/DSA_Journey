83431323bc3072664306036614cb8e22d38200b3
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-01 17:23:22 +0530

    gfg: detect cycle in undirected graph

---
+++ questions/graphs/CycleInUnDirected.java
@@ -0,0 +1,43 @@
+package questions.graph;
+
+public class CycleInUnDirected {
+    public booolean hasCycle(int numNodes, int[][] edges) {
+        Map<Integer, ArrayList<Integer>> adjList = new HashMap<>();
+
+        for(int[] i : edges) {
+            int u = i[0];
+            int v = i[1];
+
+            adjList.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+        }
+
+        boolean[] visited = new boolean[numNodes + 1];
+        int parent = -1;
+
+        for(int i = 1; i <= numNodes; i++) {
+            if(!visited[i]) {
+                if(hasCycle(i, adjList, visited, parent)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean hasCycle(int node, Map<Integer, ArrayList<Integer>> adjList, boolean[] visited, int parent) {
+        visited[node] = true;
+
+        for(int neighbor : adjList.getOrDefault(node, new ArrayList<>())) {
+            if(!visited[neighbor]) {
+                if(hasCycle(neighbor, adjList, visited, node)) {
+                    return true;
+                }
+            } else if(neighbor != parent) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
