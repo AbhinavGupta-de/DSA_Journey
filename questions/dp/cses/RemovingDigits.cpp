9048c8a5e4d799334d07ef08bb4eac2f5fa7ca46
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-29 10:13:06 +0530

    cses: removing digits

--- questions/dp/cses/RemovingDigits.cpp
+++ questions/dp/cses/RemovingDigits.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
+
 using namespace std;
 
-const int MOD = 1e9 + 7;
 long long dp[1000001];
 
 long long removingDigit(int n) {
@@ -15,19 +14,4 @@ long long removingDigit(int n) {
         ans = min(ans, 1 + removingDigit(n - (c - '0')));
     }
     return dp[n] = ans;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
-
-    cin >> n;
-    memset(dp, -1, sizeof(dp));
-
-    long long ans = removingDigit(n);
-    cout << ans << endl;
-
-    return 0;
 }
