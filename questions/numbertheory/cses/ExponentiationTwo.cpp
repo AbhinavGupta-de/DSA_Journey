ac2122da2fa16ed59ac9afce05d80086921d0c83
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-07-09 15:50:45 +0530

    cses: exponentiations two

--- questions/numbertheory/cses/ExponentiationTwo.cpp
+++ questions/numbertheory/cses/ExponentiationTwo.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 long long power(long long n, long long pow, long long MOD) {
     if (pow == 0) return 1;
 
@@ -16,18 +13,4 @@ long long ans(long long a, long long b, long long c) {
     long long MOD = 1e9 + 7;
     long long r = power(b, c, MOD - 1);
     return power(a, r, MOD) % MOD;
-}
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t-- > 0) {
-        long long a, b, c;
-        cin >> a >> b >> c;
-
-        cout << ans(a, b, c) << endl;
-    }
-
-    return 0;
 }
