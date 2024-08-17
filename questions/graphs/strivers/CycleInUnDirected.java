ee8829336c42c8af3126c109fcf9d6643a295755
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-08-17 21:51:25 +0530

    fix: fixed the issue in Cycle Detection Code

--- questions/graphs/strivers/CycleInUnDirected.java
+++ questions/graphs/strivers/CycleInUnDirected.java
@@ -1,14 +1,19 @@
 package questions.graph;
 
 public class CycleInUnDirected {
-    public booolean hasCycle(int numNodes, int[][] edges) {
-        Map<Integer, ArrayList<Integer>> adjList = new HashMap<>();
+    public int hasCycle(int numNodes, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        for(int i = 0; i <= numNodes; i++) {
+            adjList.add(new ArrayList());
+        }
 
         for(int[] i : edges) {
             int u = i[0];
             int v = i[1];
 
-            adjList.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
         }
 
         boolean[] visited = new boolean[numNodes + 1];
@@ -17,27 +22,23 @@ public class CycleInUnDirected {
         for(int i = 1; i <= numNodes; i++) {
             if(!visited[i]) {
                 if(hasCycle(i, adjList, visited, parent)) {
-                    return true;
+                    return 1;
                 }
             }
         }
 
-        return false;
+        return 0;
     }
 
-    private boolean hasCycle(int node, Map<Integer, ArrayList<Integer>> adjList, boolean[] visited, int parent) {
+    private boolean hasCycle(int node, ArrayList<ArrayList<Integer>> adjList, boolean[] visited, int parent) {
         visited[node] = true;
 
         for(int neighbor : adjList.get(node)) {
-            if(!visited[neighbor]) {
-                if(hasCycle(neighbor, adjList, visited, node)) {
-                    return true;
-                }
-            } else if(neighbor != parent) {
-                return true;
+            if(neighbor != parent){
+                if(visited[neighbor]) return true;
+                else if (hasCycle(neighbor,adjList,visited,node)) return true;
             }
         }
-
         return false;
     }
 }
